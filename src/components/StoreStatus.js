609b22b79cffcd7741263f80cd942aa3fd0ae179
Huseyin Polat <mr.huseyin.polat@gmail.com>
2024-09-05 10:52:28 +0300

    Redis config and SettingsController.java split according to brand and category

--- src/components/StoreStatus.js
+++ src/components/StoreStatus.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAllRecords } from '../api/Services.js';
-import '../css/List.css'; 
+import '../css/List.css';
 
 const StoreStatus = () => {
     const [stores, setStores] = useState([]);
@@ -16,17 +16,26 @@ const StoreStatus = () => {
         // İlgili deponun ürünlerini fetch ile alın
     };
 
+    console.log(stores);
     return (
         <div>
             <h1>Store Status</h1>
-            <ul>
+            <div className="forms-container">
+                <div className="forms-header">
+                    <div className="forms-cell">Name</div>
+                    <div className="forms-cell">Description</div>
+                    <div className="forms-cell">Notes</div>
+                </div>
+
                 {stores.map(store => (
                     // onClick={() => handleStoreClick(store)}
-                    <li key={store.id} >
-                        {store.name} - Last entry/exit: {store.lastEntryExitDate}
-                    </li>
+                    <div className="forms-row" key={store.id}>
+                        <div className="forms-cell">{store.name}</div>
+                        <div className="forms-cell">{store.description}</div>
+                        <div className="forms-cell">{store.notes}</div>
+                    </div>
                 ))}
-            </ul>
+            </div>
             {selectedStore && (
                 <div>
                     <h2>{selectedStore.name} Products</h2>
